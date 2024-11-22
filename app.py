aedec2699c41a76a519b337957bdc18c82976a9c
cathylyirang <cathylyirang@gmail.com>
2024-11-22 14:45:04 -0500

    converted python to flask

--- app.py
+++ app.py
@@ -1,21 +1,33 @@
-from flask import Flask
+from flask import Flask, request
 import random
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def guess_my_number(input):
+@app.route("/", methods=["GET", "POST"])
+def guess_my_number():
     secret = random.randint(1, 10)
-    if input == secret:
-        answer = "Congratulations, you got it!"
-    else:
-        answer = "Sorry, you missed it."
-    return answer
+
+    # If a POST request is received with user input
+    if request.method == "POST":
+        try:
+            user_guess = int(request.form.get("guess"))
+            if user_guess == secret:
+                return "Congratulations, you got it!"
+            else:
+                return f"Sorry, you missed it. The correct number was {secret}."
+        except ValueError:
+            return "Please provide a valid integer between 1 and 10."
+
+    # For GET requests, display the guessing form
+    return """
+        <form method="POST">
+            <label for="guess">Guess what integer is on my mind between 1 to 10:</label><br>
+            <input type="text" name="guess" id="guess"><br>
+            <button type="submit">Submit</button>
+        </form>
+    """
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-    guess = input("Guess what integer is on my mind between 1 to 10: ")
-    output = guess_my_number(guess)
-    print(output)
